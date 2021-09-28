7a564e382668071ba487d3ce980940ece9bb8603
alexn34-n <alexn3469@mail.ru>
2021-09-28 07:26:00 +0300

    Initial commit

---
+++ src/main/java/com/alexn/spring/springboot/spring_data_jpa/dao/EmployeeRepository.java
@@ -0,0 +1,15 @@
+package com.alexn.spring.springboot.spring_data_jpa.dao;
+
+
+
+
+
+import com.alexn.spring.springboot.spring_data_jpa.entity.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface EmployeeRepository extends JpaRepository<Employee,Integer> {
+
+    public List<Employee>  findAllByName(String name);
+}
